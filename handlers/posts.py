a97390fc92f8b487ceac93edd05ceab079e5c2e3
Dayne Wyler <dayne.wyler@utahtech.edu>
2025-10-15 10:53:12 -0600

    Added in class

--- handlers/posts.py
+++ handlers/posts.py
@@ -10,6 +10,10 @@ def get_user():
     password = flask.request.cookies.get('password')
 
     user = users.get_user(db, username, password)
+    if not user:
+            flask.flash('You need to be logged in to do that.', 'danger')
+    return user
+
 
 @blueprint.route(f'/${user}/posts', methods=['POST'])
 def post():
@@ -17,7 +21,6 @@ def post():
     db = helpers.load_db()
     user = get_user()
     if not user:
-        flask.flash('You need to be logged in to do that.', 'danger')
         return flask.redirect(flask.url_for('login.loginscreen'))
 
     post = flask.request.form.get('post')
@@ -25,10 +28,14 @@ def post():
 
     return flask.redirect(flask.url_for('login.index'))
 
-@blueprint.route(f'/${user}.posts', methods=['POST'])
+@blueprint.route(f'/${user}/contracts', methods=['POST'])
 def contracts():
     db.helpers.load_db()
     user = get_user()
+
+    if not user:
+        return flask.redirect(flask.url_for('login.loginscreen'))
+
 
     post = flask.request.form.get('post')
     posts.add_post(db, user, post)
