99db42c337bfa66a460deb86904c054c6ff30929
Dayne Wyler <dayne.wyler@utahtech.edu>
2025-10-25 12:37:30 -0600

    Slowly fixing tests

--- team_tests/test_friending.py
+++ team_tests/test_friending.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 import time
@@ -7,20 +8,32 @@ options = Options()
 options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
+options.add_argument('--disable-gpu')  # Disable GPU hardware acceleration
+options.add_argument('--disable-software-rasterizer')
+options.add_argument('--remote-debugging-port=9222')
 
 # Don't specify chromedriver path!
-driver = webdriver.Chrome(options=options)
+service = Service('/home/Dayne/bin/chromedriver')
+driver = webdriver.Chrome(options=options, service=service)
 
 #element.class works, spaces indicate descendants, < indicates when an element is directly inside of another element eg h1 > a for an anchor tag inside of an h1 tag
 
 try:
-    driver.get("http://localhost:5005/addfriend")
-    time.sleep(2)
+    driver.get("http://localhost:5005/loginscreen")
+    time.sleep(3)
 
     print("--= Beginning Tests =--")
-    add_friend_button = driver.find_element(By.CSS_SELECTOR, "button[type='submit'][value='Submit']")
-    add_friend_input_form = driver.find_element(By.CSS_SELECTOR, "input[class='form-control']")
-    add_friend_input_form_name = driver.find_element(By.CSS_SELECTOR, "p[class='card-body'] h3.card-title").text
+    username = driver.find_element(By.CSS_SELECTOR, "input[name='username']")
+    password = driver.find_element(By.CSS_SELECTOR, "input[name='password']")
+    username.send_keys("John")
+    password.send_keys("Doe")
+    login_button = driver.find_element(By.CSS_SELECTOR, "input[type='submit'][value='Create']")
+    login_button.click()
+    time.sleep(2)
+
+    add_friend_button = driver.find_element(By.CSS_SELECTOR, "div.jumbotron")
+    add_friend_input_form = driver.find_element(By.CSS_SELECTOR, "body")
+    add_friend_input_form_name = driver.find_element(By.CSS_SELECTOR, "h3[class='card-title']").text
 
     if add_friend_input_form_name == "Add Friend":
         print("[PASSED] - Add Friend Form Name is in place.")
