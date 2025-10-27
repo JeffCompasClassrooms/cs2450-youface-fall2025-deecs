cd2e4f69dbe5ac368215753be13da425d1d8448d
Dayne Wyler <dayne.wyler@utahtech.edu>
2025-10-27 08:47:02 -0600

    Small commit for compatibility

--- teamTests/test_friending.py
+++ teamTests/test_friending.py
@@ -13,8 +13,8 @@ options.add_argument('--remote-debugging-port=9222')
 
 # Don't specify chromedriver path!
 # Dayne - Selenium Manager doesn't seem to be packaged for ubuntu
-service = Service('./chromedriver')
-driver = webdriver.Chrome(options=options, service=service)
+#service = Service('./chromedriver')
+driver = webdriver.Chrome(options=options)
 
 def reset_db():
     empty_db = {"_default" : {}, "posts" : {}, "users" : {}}
