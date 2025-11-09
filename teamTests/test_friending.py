8709d526efec09ed5841cc8c2a90817b99a3f4a7
Dayne Wyler <dayne.wyler@utahtech.edu>
2025-11-08 17:46:47 -0700

    Changed URL to what everyone else was using

--- teamTests/test_friending.py
+++ teamTests/test_friending.py
@@ -72,7 +72,7 @@ successful_tests = 0
 
 try:
     driver.set_window_size(1200, 800)
-    driver.get("http://localhost:5005/login_screen")
+    driver.get("http://localhost:5005/loginscreen")
     time.sleep(1)
     print("--= Beginning Tests - Dayne Wyler =--")
 
