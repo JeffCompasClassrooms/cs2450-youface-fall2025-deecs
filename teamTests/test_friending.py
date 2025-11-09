48c18da87ad1744f8e02238e056ccc974d68d291
Dayne Wyler <dayne.wyler@utahtech.edu>
2025-11-08 17:51:49 -0700

    Removed unnecessary imports

--- teamTests/test_friending.py
+++ teamTests/test_friending.py
@@ -1,9 +1,7 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 import time
-import json
 
 options = Options()
 options.add_argument("--headless")
